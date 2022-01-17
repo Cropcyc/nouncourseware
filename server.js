224ffb0bbedf4e2d2c0fcbf393503848890f73f3
Babajide Olapade <babajidelapade@gmail.com>
2022-01-17 03:53:13 +0100

    corsOrigin made heroku/#/add

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.static(path))
 
 let corsOptions = {
-    origin: 'https://fierce-inlet-25199.herokuapp.com/'
+    origin: 'https://fierce-inlet-25199.herokuapp.com/#/add'
 }
 
 app.use(cors(corsOptions))
